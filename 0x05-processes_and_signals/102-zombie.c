20959d9a558c86bad1a24f6b043f5060dd269301
yusrA312 <yusra2yahya@gmail.com>
2024-01-26 01:04:46 -0800

    done

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - Creates an infinite loop.
+ * Return: Always returns 0.
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - Entry point of the program
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+    pid_t chi;
+    int x;
+
+    for (x = 0; x < 5; x++)
+    {
+        chi = fork();
+
+        if (chi == -1)
+        {
+            perror("Error creating child process");
+            exit(EXIT_FAILURE);
+        }
+
+        if (chi == 0)
+        {
+
+            printf("Zombie process created, PID: %d\n", getpid());
+            exit(0);
+        }
+    }
+
+    infinite_while();
+
+    return (0);
+}
