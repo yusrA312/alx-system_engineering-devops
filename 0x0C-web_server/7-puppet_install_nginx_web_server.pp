abd54facc9a7f2ea1dce8d38ab31722e3e7d4147
yusrA312 <yusra2yahya@gmail.com>
2024-03-25 15:23:32 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,24 @@
-# Puppet manifest to install and configure Nginx web server with redirection
-
-# Install Nginx package
-exec { 'update system':
-	command => '/usr/bin/apt-get update',
-}
+# install and set up nginx
 
 package { 'nginx':
-	ensure  => 'installed',
-		require => Exec['update system'],
+	ensure => 'installed',
 }
 
 file { '/var/www/html/index.html':
+	require => Package['nginx'],
 	content => 'Hello World!',
 }
 
-exec { 'redirect_me':
-	command => 'sed -i "24i\        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-		provider => 'shell',
-}
-
-file_line { 'install':
-	ensure => 'present',
-	       path   => '/etc/nginx/sites-enabled/default',
-	       after  => 'listen 80 default_server;',
-	       line   => 'rewrite ^/redirect_me https://www.github.com/yusrA312 or permanent;',
+file_line { 'add-rewrite':
+	ensure  => 'present',
+		require => Package['nginx'],
+		path    => '/etc/nginx/sites-available/default',
+		after   => 'root /var/www/html;',
+		line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+		notify  => Service['nginx'],
 }
 
 service { 'nginx':
 	ensure  => running,
-		require => Package['nginx'],
+		require => File_line['add-rewrite'],
 }
