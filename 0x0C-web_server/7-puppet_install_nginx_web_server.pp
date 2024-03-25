92fe533e8e4869cd5f0cf55487c1f5067e9e0166
yusrA312 <yusra2yahya@gmail.com>
2024-03-25 14:56:06 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,55 +1,24 @@
-# Puppet manifest to install and configure Nginx web server with redirection
+# install and set up nginx
 
-# Install Nginx package
 package { 'nginx':
-	ensure => installed,
+	ensure => 'installed',
 }
 
-# Configure Nginx server
-file { '/etc/nginx/sites-available/default':
-	ensure  => file,
-		content => "
-			server {
-				listen 80 default_server;
-				listen [::]:80 default_server;
-
-				root /var/www/html;
-				index index.html index.htm index.nginx-debian.html;
-
-				server_name _;
-
-				location / {
-					try_files $uri $uri/ =404;
-				}
-
-				location /redirect_me {
-					return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-				}
-
-				error_page 404 /error_404.html;
-				location = /error_404.html {
-					root /var/www/html;
-				}
-			}
-	",
-}
-
-# Create index.html with "Hello World!" message
 file { '/var/www/html/index.html':
-	ensure  => file,
-		content => 'Hello World!',
+	require => Package['nginx'],
+	content => 'Hello World!',
 }
 
-# Create custom error page
-file { '/var/www/html/error_404.html':
-	ensure  => file,
-		content => "Ceci n'est pas une page",
+file_line { 'add-rewrite':
+	ensure  => 'present',
+		require => Package['nginx'],
+		path    => '/etc/nginx/sites-available/default',
+		after   => 'root /var/www/html;',
+		line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+		notify  => Service['nginx'],
 }
 
-# Ensure Nginx service is running and enabled
 service { 'nginx':
 	ensure  => running,
-		enable  => true,
-		require => File['/etc/nginx/sites-available/default'],
+		require => File_line['add-rewrite'],
 }
-
