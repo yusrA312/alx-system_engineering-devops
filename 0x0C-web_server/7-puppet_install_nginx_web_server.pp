2d81388efc4ffa6dd138c95f0269d48830cf2c73
yusrA312 <yusra2yahya@gmail.com>
2024-03-25 14:59:46 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,27 @@
-# install and set up nginx
+# Puppet manifest to install and configure Nginx web server with redirection
 
+# Install Nginx package
 package { 'nginx':
-	ensure => 'installed',
+	ensure => installed,
 }
 
+# Create index.html with "Hello World!" message
 file { '/var/www/html/index.html':
-	require => Package['nginx'],
-	content => 'Hello World!',
+	ensure  => file,
+		content => 'Hello World!',
+		require => Package['nginx'],
 }
 
-file_line { 'add-rewrite':
-	ensure  => 'present',
-		require => Package['nginx'],
+# Configure redirection in Nginx configuration file
+file_line { 'add-rewrite-rule':
+	ensure  => present,
 		path    => '/etc/nginx/sites-available/default',
-		after   => 'root /var/www/html;',
-		line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+		line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+		require => Package['nginx'],
 		notify  => Service['nginx'],
 }
 
+# Ensure Nginx service is running
 service { 'nginx':
-	ensure  => running,
-		require => File_line['add-rewrite'],
+	ensure => running,
 }
