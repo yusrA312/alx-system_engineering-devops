ff802ba67e554c399f0c48aaef5e740638982ebe
yusrA312 <yusra2yahya@gmail.com>
2024-04-22 04:56:00 -0700

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,8 +7,7 @@ from sys import argv
 if __name__ == '__main__':
     user = argv[1]
     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    r = requests.get(url_user)
-    
+    r = requests.get(url_user)    
     user_name = r.json().get('username')
     task = url_user + '/todos'
     r = requests.get(task)
@@ -19,5 +18,4 @@ if __name__ == '__main__':
             completed = task.get('completed')
 
             title_t = task.get('title')
- 
             vfile.write(f'"{user}","{user_name}","{completed}","{title_t}"\n')
