df16d59f72d456855f1a20e391b3e24b4c19064f
yusrA312 <yusra2yahya@gmail.com>
2024-05-07 06:22:54 -0700

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,8 @@ Function to cout  subreddit.
 
 import requests
 
-def count_words(subreddit, word_list, after='', word_frequency=None):
+
+def count_words(subreddit, word_list, after="", word_frequency=None):
     """A function that queries the Reddit API recursively, parses the title of
     all hot articles, and prints a sorted count of given keywords.
     """
@@ -13,25 +14,27 @@ def count_words(subreddit, word_list, after='', word_frequency=None):
         word_frequency = {word.lower(): 0 for word in word_list}
 
     if after is None:
-        sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: (-x[1], x[0]))
+        sorted_word_frequency = sorted(
+            word_frequency.items(), key=lambda x: (-x[1], x[0])
+        )
         for word, count in sorted_word_frequency:
             if count:
                 print(f"{word}: {count}")
         return
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'redquery'}
-    params = {'limit': 100, 'after': after}
-    
+    headers = {"User-Agent": "redquery"}
+    params = {"limit": 100, "after": after}
+
     try:
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
-        data = response.json().get('data', {})
-        posts = data.get('children', [])
-        after = data.get('after')
+        data = response.json().get("data", {})
+        posts = data.get("children", [])
+        after = data.get("after")
 
         for post in posts:
-            post_title = post['data']['title']
+            post_title = post["data"]["title"]
             words = post_title.lower().split()
             for word in word_list:
                 word_frequency[word.lower()] += words.count(word.lower())
