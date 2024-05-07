774511532729a6717f80dfb6569c2fa75bb6ab6b
yusrA312 <yusra2yahya@gmail.com>
2024-05-07 05:31:37 -0700

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,6 +2,7 @@
 """This is the request """
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     """A function that queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit. If no results
