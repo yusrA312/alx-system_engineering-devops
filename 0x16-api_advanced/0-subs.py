e47365cd124c907284097f52ddef2299bb249429
yusrA312 <yusra2yahya@gmail.com>
2024-05-07 02:54:56 -0700

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""This is the module to handle requests for sending
-HTTP requests to the Reddit API"""
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """This is function that queries the Reddit API
-    and returns the number of subscribers"""
-    if not subreddit or type(subreddit) is not str:
-        return 0
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        r = r.json()
-    else:
-        return 0
-    return r.get('data', {}).get('subscribers', 0)
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
