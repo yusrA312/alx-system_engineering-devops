2f930a1c9f275e0a62a1b57d2ee317f1d70c8633
yusrA312 <yusra2yahya@gmail.com>
2024-05-07 15:00:56 -0700

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""This is the request """
+""" 2-recurse.py"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], yy=None):
-    """A function"""
-    U = f"http://www.reddit.com/r/{subreddit}/hot.json"
-    head = {"User-Agent": "0x16-api_advanced:project:v1.0.0"}
-    params = {"limit": 100}
-    if yy is not None:
-        params["yy"] = yy
+def recurse(subreddit, hot_list=[], after=None):
+    """Queries the Reddit API"""
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    HEADERS = {'User-Agent': 'Hi/0.'}
+    PARAMS = {'after': after} if after else {}
 
-    try:
-        r = requests.get(U, head=head, params=params)
-        r.raise_for_status()
-        data = r.json().get("data", {})
-        posts = data.get("children", [])
-        if not posts:
-            return hot_list
-        hot_list.extend([post["data"]["title"] for post in posts])
-        new_yy = data.get("yy")
-        return recurse(subreddit, hot_list, new_yy)
-    except requests.RequestException:
+    R = requests.get(URL, headers=HEADERS, params=PARAMS)
+    if R.status_code == 200:
+        DATA = R.json()
+        POSTS = DATA['data']['children']
+        if POSTS:
+            hot_list.extend([post['data']['title'] for post in POSTS])
+            AFTER = DATA['data']['after']
+            if AFTER:
+                return recurse(subreddit, hot_list, AFTER)
+            else:
+                return hot_list
+        else:
+            return None
+    else:
         return None
