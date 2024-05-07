052eb498ba76b605b4a49bb07cdaf3f2f17aa2ed
yusrA312 <yusra2yahya@gmail.com>
2024-05-07 06:21:58 -0700

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
+"""
+Function to cout  subreddit.
+"""
 
 import requests
 
-
-def count_words(subreddit, word_list, after="", word_frequency=None):
-    """A function that queries the Reddit API.
+def count_words(subreddit, word_list, after='', word_frequency=None):
+    """A function that queries the Reddit API recursively, parses the title of
+    all hot articles, and prints a sorted count of given keywords.
     """
     if word_frequency is None:
         word_frequency = {word.lower(): 0 for word in word_list}
 
     if after is None:
-        sorted_word_frequency = sorted(
-            word_frequency.items(), key=lambda x: (-x[1], x[0])
-        )
+        sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: (-x[1], x[0]))
         for word, count in sorted_word_frequency:
             if count:
                 print(f"{word}: {count}")
         return
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "redquery"}
-    params = {"limit": 100, "after": after}
-
+    headers = {'User-Agent': 'redquery'}
+    params = {'limit': 100, 'after': after}
+    
     try:
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
-        data = response.json().get("data", {})
-        posy = data.get("children", [])
-        after = data.get("after")
+        data = response.json().get('data', {})
+        posts = data.get('children', [])
+        after = data.get('after')
 
-    for post in posts:
-        title = post["data"]["title"].lower()
-        words = title.split()
-        word_frequency.update({word.lower(): word_frequency.get(word.lower(), 0) + words.count(word.lower()) for word in word_list})
+        for post in posts:
+            post_title = post['data']['title']
+            words = post_title.lower().split()
+            for word in word_list:
+                word_frequency[word.lower()] += words.count(word.lower())
 
-        
     except requests.RequestException:
         return None
     except Exception:
