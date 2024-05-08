c524fd5e2796cdc4b5b6a07de2dc723e76c9016a
yusrA312 <yusra2yahya@gmail.com>
2024-05-08 15:47:13 -0700

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-# 0-strace_is_your_friend.pp
-
-package {
- 'libapache2-mod-php':
-  ensure => installed,
+# puppet file to automate a 500 error fix
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin';
 }
-
-# Define a service resource to ensure Apache is restarted after the fix
-service {
- 'apache2':
-  ensure => running,
-  enable => true,
-  require => Package['libapache2-mod-php'],
-}
-=======
-# fixing Apache is returning a 500 error
-
-exec { 'fix_apache_error_config':
-        command  => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
-        provider => 'shell'
-}
->>>>>>> 755381b5e999927009f3053f59c2d6eb6aafd233
