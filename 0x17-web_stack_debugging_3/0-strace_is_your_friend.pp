9306771429fe59863eb0c62df3c2bc978e04fc97
yusrA312 <yusra2yahya@gmail.com>
2024-05-08 15:45:06 -0700

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,23 @@
-file_line { 'fix-php-in-wp-settings':
-  ensure  => present,
-  path    => '/var/www/html/wp-settings.php',
-  line    => 'require_once( ABSPATH . WPINC . "/phpp/default-constants.php" );',
-  match   => '^require_once\( ABSPATH \. WPINC \. "/phpp/default-constants\.php" \);',
+<<<<<<< HEAD
+# 0-strace_is_your_friend.pp
+
+package {
+ 'libapache2-mod-php':
+  ensure => installed,
 }
+
+# Define a service resource to ensure Apache is restarted after the fix
+service {
+ 'apache2':
+  ensure => running,
+  enable => true,
+  require => Package['libapache2-mod-php'],
+}
+=======
+# fixing Apache is returning a 500 error
+
+exec { 'fix_apache_error_config':
+        command  => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
+        provider => 'shell'
+}
+>>>>>>> 755381b5e999927009f3053f59c2d6eb6aafd233
