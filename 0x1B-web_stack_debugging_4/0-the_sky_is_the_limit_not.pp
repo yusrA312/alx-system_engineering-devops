7f11987f842d3d6b044c7be3b865c791e716edb0
yusrA312 <yusra2yahya@gmail.com>
2024-05-16 03:55:30 -0700

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Increase the ULIMIT in order to increase the amount of traffic an Nginx server can handle
+# The sky is the limit to your requests here
 
 exec { 'change_default_ulimit':
-	command  => 'sed -i "s/15/2000/" /etc/default/nginx',
-		 provider => 'shell',
-		 notify   => Exec['restart_nginx_service']
+  command  => 'sed -i "s/15/2000/" /etc/default/nginx',
+  provider => 'shell',
+  notify   => Exec['restart_nginx_service']
 }
 
 exec { 'restart_nginx_service':
-	command     => 'sudo service nginx restart',
-		    provider    => 'shell',
-		    refreshonly => true
+  command     => 'sudo service nginx restart',
+  provider    => 'shell',
+  refreshonly => true
 }
